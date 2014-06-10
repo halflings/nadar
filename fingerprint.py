ea2e6efe8339428d260060fb6208278d168042cf
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-06-09 22:18:36 -0700

    Changed hash collection structure, new AppSignature class

--- fingerprint.py
+++ fingerprint.py
@@ -1,48 +1,56 @@
+import argparse
 import hashlib
 import json
-import sys
 import os
+from collections import defaultdict
 
-class DirState(object):
-
+class AppSignature(object):
     def __init__(self, path):
         self.path = path
+        self.signatures = dict()
+        _, top_level_dirs, _ = os.walk(self.path).next()
+        for folder in top_level_dirs:
+            folder_path = os.path.join(self.path, folder)
+            self.signatures[folder] = DirSignature(folder_path)
 
-        self.file_hashes = dict()
+            # DEBUG
+            print folder
+            print
+            print self.signatures[folder].json()
+            print '=' * 80
+
+class DirSignature(object):
+
+    def __init__(self, path, hash_func=hashlib.md5):
+        self.path = path
+        self.folder_name = os.path.split(self.path)[-1]
+
+        self.hashes = defaultdict(dict)
         self.files = set()
-
-        self.dir_hashes = dict()
-
-
 
         for root, dirs, files in os.walk(path):
             self.files.update(files)
-
-            folder_rel_path = root[len(self.path):]
-            dir_hash = hashlib.md5()
-
-
-            # folder_tokens = folder_rel_path.split('/')
-            # parent_dirs = ['']
 
             for filename in files:
                 file_path = os.path.join(root, filename)
                 file_rel_path = file_path[len(self.path):]
                 with open(file_path) as file:
                     file_content = file.read()
-                self.file_hashes[file_rel_path] = hashlib.md5(file_content).hexdigest()
-                dir_hash.update(file_content)
-
-            self.dir_hashes[folder_rel_path] = dir_hash.hexdigest()
+                file_hash = hash_func(file_content).hexdigest()
+                self.hashes[file_rel_path][file_hash] = self.folder_name
 
     def json(self, indent=4):
-        output = dict(files=self.file_hashes, dirs=self.dir_hashes)
+        output = dict(files=self.hashes)
         return json.dumps(output, indent=indent)
 
     def __str__(self):
-        return "<Dir: '{}' - {} files>".format(self.path, len(self.file_hashes))
+        return "<Dir: '{}' - {} files>".format(self.path, len(self.hashes))
 
 
 if __name__ == '__main__':
-    src_dir = sys.argv[1]
-    print DirState(src_dir).json()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('path', help="path to the source directory")
+    args = parser.parse_args()
+    src_dir = args.path
+
+    AppSignature(src_dir)
